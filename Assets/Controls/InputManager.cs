328f869e18b3cdbd415a75429d9075777cbff535
Akenaten <35400816+Akenaten@users.noreply.github.com>
2023-10-29 14:00:31 +0200

    Camera fixed sloggy follow and inversion. Player movement set to relative to camera.

--- Assets/Controls/InputManager.cs
+++ Assets/Controls/InputManager.cs
@@ -11,6 +11,11 @@ public class InputManager : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] float movementSpeed;
 
+    public Transform cam;
+
+    [TextArea(5,10)]
+    public string DebugTextArea;
+
     private void Awake() {
         input = new PlayerControls();
     }
@@ -36,7 +41,20 @@ public class InputManager : MonoBehaviour
     private void FixedUpdate() {
 
         //NEEDS EDIT. INPUT RECEIVES X,Y AND WE MAP TO THE X, Z VALUES OF VELOCITY. LOOK FOR SHORTER, MORE DIRECT WAY.
-        rb.velocity = new Vector3(inputVector2.x, rb.velocity.y, inputVector2.y) * movementSpeed;
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+        camForward.y = camRight.y = 0;
+
+        Vector3 forwardRelative = inputVector2.y * camForward;
+        Vector3 rightRelative = inputVector2.x * camRight;
+
+        Vector3 moveDir = forwardRelative + rightRelative;
+
+        DebugTextArea = $"Forward Relative: {forwardRelative}\n Right Relative: {rightRelative}\n Move Direction: {moveDir}";
+
+        // rb.velocity = new Vector3(inputVector2.x, rb.velocity.y, inputVector2.y) * movementSpeed;
+        // rb.velocity = new Vector3(moveDir.y, rb.velocity.y, moveDir.x) * movementSpeed;
+        transform.Translate(moveDir.normalized * Time.deltaTime * movementSpeed, Space.World);
     }
     
 }
